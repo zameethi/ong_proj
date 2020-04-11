3dd7361728c6369f315a6173f7a94d6a4fc5b6b3
zamee <andre.zigaib@gmail.com>
2020-04-11 10:39:57 -0300

    update page

--- despesa/urls.py
+++ despesa/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('despesa', despesa,  name='despesa'),
+    path('despesa/<int:pk>', editar_despesas,  name='atualiza'),
     path("lista/", lista_Despesa.as_view()),
     ]
